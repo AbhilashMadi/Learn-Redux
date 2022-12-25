798a4cc8856d9aee64cee911765a4d3df42a6056
Abhilash Madi <madiabhilash1563@gmail.com>
2022-12-25 07:13:42 +0530

    working

--- Code/index.js
+++ Code/index.js
@@ -1,1 +1,27 @@
-console.log("Hello redux...")
+//Reducer
+
+const BUY_CAKE = "BUY_CAKE"
+
+function buyCake(){
+    return {
+        type: BUY_CAKE,
+        info:"First redux action"
+    }
+}
+
+//(previousState, action) => newState()
+
+const intitalState = {
+    numOfCakes: 10
+}
+
+const reducer = (state=intitalState,action) => {
+    switch(action.type){
+        case BUY_CAKE: return {
+            ...state,
+            numOfCakes: state.numOfCakes - 1
+        }
+
+        default: return state;
+    }
+}
